37c042fc7676a6b8f94a403d28fda70db38654c9
Kevin Aldrich <kevin.aldrich@gmail.com>
2017-01-22 23:16:05 -0800

    Added click event

--- _js/scripts.js
+++ _js/scripts.js
@@ -1,15 +1,22 @@
-var highPass = function highPass(number, cutoff) {
-    if (number >= cutoff) {
-      return true;
-    } else {
-      return false;
+function clickSquare(e) {
+    console.log(e);
+    if (e.target.className == "gameCellText") {
+        alert("Please choose an empty square");
+        return;
     }
-  },
+    var cell = document.getElementsByClassName(e.target.className);
+    console.log(cell);
+    var cellText = cell[0].getElementsByClassName("gameCellText");
+    console.log(cellText[0]);
+    cellText[0].innerHTML = "X";
+}
 
-  lowPass = function lowPass(number, cutoff) {
-    if (number >= cutoff) {
-      return true;
-    } else {
-      return false;
+document.onreadystatechange = function () {
+  if (document.readyState === "complete") {
+    var elements = document.getElementsByClassName("gameCell");
+    for (e of elements) {
+        e.addEventListener("click", clickSquare);
     }
-  };
+    // initApplication();
+  }
+};
