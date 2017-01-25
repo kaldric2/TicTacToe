4c1a1eb5a99d04ebcf596b68f33a55a4d79c0132
Kevin Aldrich <kevin.aldrich@gmail.com>
2017-01-24 23:50:12 -0800

    Implemented basic game logic

--- _js/scripts.js
+++ _js/scripts.js
@@ -1,13 +1,14 @@
 /* jshint esversion: 6 */
+/* jshint expr: true */
 
 // Handle 1-player vs 2-player
 // Toggle game state on button click
     // Change button text
     // Swap .playerSelection with .playerScores
 // Random number generator to see who goes first
-// TODO: Loser goes first
-// TODO: Keep track of whose turn it is
-// TODO: Keep track of the letter to place in the square
+// Loser goes first
+// Keep track of whose turn it is
+// Keep track of the letter to place in the square
 // TODO: Build game logic for cpu AI
 // TODO: Build function to check for win condition
 // TODO: Make a custom alert box & function
@@ -18,26 +19,103 @@
 // globals
 // const xo = ["X", "O"];
 var onePlayer = false;
-var p1 = { name: "Player One" }, p2 = { name: "Player Two" };
-var firstTurn = 1; // (Player) 1 or 2
+var playersObj = {
+    p1: { name: "Player One" },
+    p2: { name: "Player Two" }
+};
+var firstTurn = 0; // (Player) 1 or 2
 var curTurn = 1; // (Player) 1 or 2
 var squaresArray = [];
 var matchScores = [0,0];
 
+function randomSquare() {
+    // if using 2d squaresArray
+        // var x = Math.floor(Math.random() * 3) + 1;
+        // var y = Math.floor(Math.random() * 3) + 1;
+        // return [x,y];
+    // if using 1d squaresArray
+    return Math.floor(Math.random() * 8);
+}
+
 function cpuMove() {
-    // TODO: Build cpu AI
+    // TODO: Build non-random cpu AI
+    var emptySquares = false;
+    while (!emptySquare) {
+        // if using 2d squaresArray
+            // var square = randomSquare();
+            // if (squaresArray[square[0]][square[1]] == "") {
+            //     emptySquare = true;
+            // }
+        // if using 1d squaresArray
+        var square = squaresArray[randomSquare()];
+        if (square.firstChild.innerHTML === "") {
+            emptySquare = true;
+            square.firstChild.innerHTML = playersObj.p2.marker;
+        }
+    }
 
     // After move, check for win condition
     checkWin();
-    return;
+}
+
+function updatePlayerText(curPlayer, lastPlayer) {
+    if (lastPlayer) {
+        document.getElementsByName(lastPlayer)[0].style = "";
+    }
+
+    document.getElementsByName(curPlayer)[0].style.color = "#fff";
+}
+
+function nextMove() {
+    if (onePlayer && curTurn === 2) {
+        updatePlayerText("p2Name", "p1Name");
+        // Wait one second before moving
+        window.setTimeout(cpuMove, 1000);
+    } else {
+        var lastTurn;
+        curTurn === 1 ? lastTurn = 2 : lastTurn = 1;
+        var lastPlayer = "p" + lastTurn + "Name";
+        var curPlayer = "p" + curTurn + "Name";
+        updatePlayerText(curPlayer, lastPlayer);
+    }
+}
+
+function gameOver() {
+    // flip firstTurn, increment matchScores
+    if (curTurn === 1) {
+        firstTurn = 2;
+        matchScores[0]++;
+    } else {
+        firstTurn = 1;
+        matchScores[1]++;
+    }
+
+    // check to see if match has been won
+    for (var i = 0; i < matchScores.length; i++) {
+        if (matchScores[i] === 3) {
+            matchOver(i + 1);
+            return;
+        }
+    }
+
+    // if not, start a new game
+    startGame();
 }
 
 function checkWin() {
     // TODO: check for win condition
+    var win = false;
+
     // If win, set firstTurn = losing player
-    // If no win, flip active player and trigger AI (if one player)
-    cpuMove();
-    return;
+    if (win) {
+
+    } else {
+        // If no win, flip active player and trigger next move
+        curTurn === 1
+            ? curTurn = 2
+            : curTurn = 1;
+            nextMove();
+    }
 }
 
 function clickSquare(square) {
@@ -47,11 +125,10 @@ function clickSquare(square) {
         alert("Please choose an empty square");
         return;
     }
-    var cell = document.getElementsByClassName(square.target.className);
-    var cellText = cell[0].getElementsByClassName("gameCellText");
-    // TODO: add logic to determine "X" or "O"
+
     var curPlayer = "p" + curTurn;
-    cellText[0].innerHTML = curPlayer.marker;
+    var cellText = square.target.firstChild;
+    cellText.innerHTML = playersObj[curPlayer].marker;
 
     // Check for win condition
     checkWin();
@@ -69,7 +146,7 @@ function setMarkers(xPlayer, oPlayer) {
 
 function startGame() {
     // If start of match, decide who goes first
-    if (matchScores.reduce(function(a,v) { return a + v; }) === 0) {
+    if (!firstTurn) {
         goesFirst();
     }
 
@@ -77,7 +154,9 @@ function startGame() {
     curTurn = firstTurn;
 
     // Set player markers
-    firstTurn === 1 ? setMarkers(p1, p2) : setMarkers(p2, p1);
+    firstTurn === 1
+        ? setMarkers(playersObj.p1, playersObj.p2)
+        : setMarkers(playersObj.p2, playersObj.p1);
 
     // Change button text to "New Match"
     document.getElementsByName("newStartButton")[0].setAttribute("value","New Match");
@@ -88,11 +167,28 @@ function startGame() {
     // Unhide .playerScores
     document.getElementsByClassName("playerScores")[0].style.display = "block";
 
+    // Update .playerScores
+    document.getElementsByClassName("p1Score")[0].innerHTML = matchScores[0];
+    document.getElementsByClassName("p2Score")[0].innerHTML = matchScores[1];
+
     // Add event listeners to .gameCell
+    // Empty gameCellTexts
     var squares = document.getElementsByClassName("gameCell");
     for (var e of squares) {
-        e.innerHTML = "";
+        e.firstChild.innerHTML = "";
         e.addEventListener("click", clickSquare);
+    }
+
+    // Fill squaresArray with gameCellTexts
+    squaresArray = document.getElementsByClassName("gameCell");
+
+    if (onePlayer && curTurn === 2) {
+        updatePlayerText("p2Name");
+        cpuMove();
+    } else {
+        curTurn === 1
+            ? updatePlayerText("p1Name")
+            : updatePlayerText("p2Name");
     }
 }
 
@@ -103,7 +199,7 @@ function resetGame() {
     document.getElementsByName("newStartButton")[0].setAttribute("value","Start Match");
 
     // Clear match scores
-    matchScores = [];
+    matchScores = [0,0];
 
     // Clear squares array
     squaresArray = [];
@@ -129,9 +225,9 @@ function initGame(btn) {
         var players = document.getElementsByName("players");
         for (var p of players) {
             if (p.checked) {
-                if (p.value === 1) {
+                if (p.value == 1) {
                     onePlayer = true;
-                    p2.name = "Computer";
+                    playersObj.p2.name = "Computer";
                     document.getElementsByName("p2Name")[0].setAttribute("value","Computer");
                 }
                 startGame();
